89258267ee5439e773c12c6a6a1ecfbf6a9754e3
JoshuaXCheung <Joshua.Cheung@queensu.ca>
2021-05-08 02:59:03 -0400

    Fridge Page

--- src/fridge.js
+++ src/fridge.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function FridgePage() {
   return (
     <View style={styles.container}>
+      <Image style={styles.Logo} source={require('./Images/Logo.png')}></Image>
       <Text style={styles.appname}>Hi </Text>
       <Text style={styles.appname}>Bye </Text>
       <TouchableOpacity>
           <Text>Button</Text>
       </TouchableOpacity>
+      <Text style={styles.MyFridgeName}>My Fridge </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,6 +24,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Logo: {
+    position: 'absolute',
+    width:130,
+    height: 130,
+    alignItems: 'center',
+    top: 77,
+  },
+  MyFridgeName: {
+    position: 'absolute',
+    textAlign: 'center',
+    top: 180,
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    fontFamily: 'Lato',
+
+
+
+  },
   appname:{
-    fontSize: 80,}
-});
+    fontSize: 90,}
+}); 
+
