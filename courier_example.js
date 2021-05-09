46a9637104d92740b107ad69d772e68639be61dd
Here4Quantum <79421530+Here4Quantum@users.noreply.github.com>
2021-05-09 17:39:44 +0100

    Add files via upload

---
+++ courier_example.js
@@ -0,0 +1,23 @@
+import { CourierClient } from "@trycourier/courier";
+
+const courier = CourierClient({ authorizationToken: "******************" });
+
+// Example: send a message supporting email & SMS
+const { messageId } = await courier.send({
+  eventId: "PS4NEM8MPF44X2K40Z6NMGAMXY6T",
+  recipientId: "239fec2a-f053-43a5-bc43-b90c039af5d4",
+  profile: {
+    phone_number: "**********",
+  },
+  data: {
+    username: "Joshua",
+    food_item: [
+      "Green beans: 7 days",
+      "Cheese: 10 days",
+      "Orange Juice: 3 days",
+      "Milk: 4 days",
+    ],
+  },
+  override: {
+  },
+});
