46995e88d0cf10c199074e1341f76d68499cb623
jenniferchen11 <jennifer.chen2002@gmail.com>
2021-05-09 17:36:06 -0400

    Update courier_notif.js

--- courier_notif.js
+++ courier_notif.js
@@ -2,7 +2,7 @@ import { CourierClient } from "@trycourier/courier";
 
 const courier = CourierClient({ authorizationToken: "******************" });
 
-// Example: send a message supporting email & SMS
+// Sending a message (email & SMS) with courier API
 const { messageId } = await courier.send({
   eventId: "PS4NEM8MPF44X2K40Z6NMGAMXY6T",
   recipientId: "239fec2a-f053-43a5-bc43-b90c039af5d4",
